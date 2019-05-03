71fed5a7d4a492af88f4cfa0772c4e7ac75144cd
Sylvain Gaultier <sylvain.gaultier@actronika.com>
2019-05-03 17:59:14 +0200

    smoke manager WIP

--- low_level/SmokeMgr.h
+++ low_level/SmokeMgr.h
@@ -1,6 +1,19 @@
 #ifndef SMOKE_MGR_h
 #define SMOKE_MGR_h
 
+#include <Arduino.h>
+#include "Singleton.h"
+#include "Config.h"
 
+class SmokeMgr : public Singleton<SmokeMgr>
+{
+public:
+    SmokeMgr()
+    {
+        pinMode(PIN_SMOKE_PUMP, OUTPUT);
+        pinMode(PIN_SMOKE_RESISTOR, OUTPUT);
+    }
+
+};
 
 #endif
