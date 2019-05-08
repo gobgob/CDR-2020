4b61303dc08cc3717506254935c4ad0dd7e6aa90
PF <pierre-francois.gimenez@laposte.net>
2019-05-08 14:59:59 +0200

    Fix script accélérateur

--- high_level/src/main/java/senpai/scripts/ScriptAccelerateur.java
+++ high_level/src/main/java/senpai/scripts/ScriptAccelerateur.java
@@ -61,7 +61,7 @@ public class ScriptAccelerateur extends Script
 		try {
 			robot.execute(CommProtocol.Id.ACTUATOR_GO_TO, -23.7, 152., 0.);
 			robot.avanceTo(new XYO(-135, 1740, Math.PI / 2));
-			robot.execute(CommProtocol.Id.ACTUATOR_GO_TO_AT_SPEED, 23.7, 225., 0., 350., 300., 1023.);
+			robot.execute(CommProtocol.Id.ACTUATOR_GO_TO_AT_SPEED, 23.7, 190., 0., 350., 300., 1023.);
 			robot.updateScore(20);
 			robot.setGoldeniumFree();
 			// si tout s'est bien passé, alors le script n'est plus faisable
