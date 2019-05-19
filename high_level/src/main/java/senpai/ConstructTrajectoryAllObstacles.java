55b68230128bc4c408e8e315480fb8dbf5bbdf0a
PF <pierre-francois.gimenez@laposte.net>
2019-05-19 21:21:54 +0200

    Obstacle de table dans contruct trajectory

--- high_level/src/main/java/senpai/ConstructTrajectoryAllObstacles.java
+++ high_level/src/main/java/senpai/ConstructTrajectoryAllObstacles.java
@@ -8,37 +8,40 @@ import pfg.kraken.Kraken;
 import pfg.kraken.SearchParameters;
 import pfg.kraken.astar.DirectionStrategy;
 import pfg.kraken.exceptions.PathfindingException;
+import pfg.kraken.obstacles.RectangularObstacle;
 import pfg.kraken.robot.ItineraryPoint;
 import pfg.kraken.utils.XY;
 import pfg.kraken.utils.XYO;
+import pfg.kraken.utils.XY_RW;
 import pfg.log.Log;
 import senpai.Senpai.ErrorCode;
 import senpai.robot.KnownPathManager;
 import senpai.robot.SavedPath;
+import senpai.table.Table;
 import senpai.utils.Subject;
 
 public class ConstructTrajectoryAllObstacles
 {
 	public static void main(String[] args) throws InterruptedException
 	{
-		if(args.length < 8 || args.length > 10)
+		if(args.length < 9 || args.length > 11)
 		{
-			System.out.println("Usage : ./run.sh "+ConstructTrajectory.class.getSimpleName()+" mode vitesse_max deploye x_depart y_depart o_depart x_arrivee y_arrivee [o_arrivee] [chemin]");
+			System.out.println("Usage : ./run.sh "+ConstructTrajectory.class.getSimpleName()+" mode vitesse_max couleur deploye x_depart y_depart o_depart x_arrivee y_arrivee [o_arrivee] [chemin]");
 			return;
 		}
 		
 		boolean modeXY = args[0].equals("XY");
 		
 		double vitesseMax = Double.parseDouble(args[1]) / 1000.;
-		
-		boolean deploye = Boolean.parseBoolean(args[2]);
-		double x = Double.parseDouble(args[3]);
-		double y = Double.parseDouble(args[4]);
-		double o = Double.parseDouble(args[5]);
+		boolean symetrie = Boolean.parseBoolean(args[2]);
+		boolean deploye = Boolean.parseBoolean(args[3]);
+		double x = Double.parseDouble(args[4]);
+		double y = Double.parseDouble(args[5]);
+		double o = Double.parseDouble(args[6]);
 		XYO depart = new XYO(x,y,o);
 		
-		x = Double.parseDouble(args[6]);
-		y = Double.parseDouble(args[7]);
+		x = Double.parseDouble(args[7]);
+		y = Double.parseDouble(args[8]);
 		
 		XY arriveeXY = null;
 		XYO arriveeXYO = null;
@@ -46,18 +49,25 @@ public class ConstructTrajectoryAllObstacles
 			arriveeXY = new XY(x,y);
 		else
 		{
-			o = Double.parseDouble(args[8]);
+			o = Double.parseDouble(args[9]);
 			arriveeXYO = new XYO(x,y,o);
 		}
 		
 		String output = null;
-		if(modeXY && args.length > 8)
-			output = args[8];
-		else if(!modeXY && args.length > 9)
-			output = args[9];		
+		if(modeXY && args.length > 9)
+			output = args[9];
+		else if(!modeXY && args.length > 10)
+			output = args[10];		
 		
 		Senpai senpai = new Senpai();
 		senpai.initialize("senpai-trajectory.conf", "default", "graphic");
+		Table table = senpai.getService(Table.class);
+		
+		XY_RW posZoneDepartAdverse = new XY_RW(-1500+550/2, 1250);
+		if(symetrie)
+			posZoneDepartAdverse.setX(- posZoneDepartAdverse.getX());
+		table.addOtherObstacle(new RectangularObstacle(posZoneDepartAdverse, 550, 900));
+
 		Kraken k = senpai.getService(Kraken[].class)[deploye ? 0 : 1];
 		Log log = senpai.getService(Log.class);
 		GraphicDisplay display = senpai.getService(GraphicDisplay.class);
