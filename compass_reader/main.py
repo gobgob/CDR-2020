360ba945a6f0260b35339a64968fd11924199d7d
Fabien BERNARD <fabien0102@hotmail.com>
2019-09-29 14:27:46 +0200

    Cleanup + improve thread management

--- compass_reader/main.py
+++ compass_reader/main.py
@@ -1,15 +1,26 @@
 from aiohttp import web
 from compass import CompassWatcher
 
-compass_watcher = CompassWatcher()
-
 
 async def get_compass_value(request):
+    compass_watcher = request.app['compass_watcher']
+
+    if compass_watcher.error:
+        return web.json_response(status=500, data={"message": compass_watcher.error})
+
     return web.json_response(compass_watcher.get_value())
+
+
+async def start_background_task(app):
+    app['compass_watcher'] = CompassWatcher()
+
+
+async def cleanup_background_task(app):
+    app['compass_watcher'].stop()
 
 
 app = web.Application()
 app.add_routes([web.get('/', get_compass_value)])
-
-if __name__ == '__main__':
-    web.run_app(app)
+app.on_startup.append(start_background_task)
+app.on_cleanup.append(cleanup_background_task)
+web.run_app(app)
